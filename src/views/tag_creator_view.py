2786fb4038505bc37940fcf50da640a8bef40121
Pedro Martins <pedromartinsbda@gmail.com>
2024-02-08 19:05:34 -0300

    feat: implementing view for creator with http types

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -7,10 +7,11 @@ class TagCreatorView:
     '''
 
     def validate_and_create(self, http_request: HttpRequest):
-        body = http_request.body
-        product_code = body["product_code"]
+        # body = http_request.body
+        # product_code = body["product_code"]
 
         # logica de regra de negocio
         print('estou na minha view')
+        print(http_request)
         # retorno http
         return HttpResponse(status_code=200, body={"hello": "world"})
