90f79f3b2aefcd3e694f043aa1b62d910e47829a
Pedro Martins <pedromartinsbda@gmail.com>
2024-02-10 20:53:10 -0300

    feat: implementing validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,6 +1,23 @@
 from src.views.http_types.http_response import HttpResponse
+from error_types.http_unprocessable_entity import HttpUnprocessableEntityError
+
+
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        # enviar para um log
+        # enviar um email de notificação
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "errors": [{
+                    "title": error.name,
+                    "datail": error.message
+                }]
+            }
+        )
+
+
     return HttpResponse(
         status_code=500,
         body={
